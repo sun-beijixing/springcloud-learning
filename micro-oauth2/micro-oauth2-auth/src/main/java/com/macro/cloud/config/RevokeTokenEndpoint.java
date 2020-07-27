2f8ec22fe181e341b53ee940f0b76ed552fb69cf
wangwei.0822@163.com <wangwei.0822@163.com>
2020-07-27 09:35:29 +0800

    添加注销

---
+++ micro-oauth2/micro-oauth2-auth/src/main/java/com/macro/cloud/config/RevokeTokenEndpoint.java
@@ -0,0 +1,36 @@
+package com.macro.cloud.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.endpoint.FrameworkEndpoint;
+import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @ClassName RevokeTokenEndpoint
+ * @Description TODO
+ * @Author wangwei.0822@163.com
+ * @Data 2020/7/24 18:50
+ **/
+@FrameworkEndpoint
+public class RevokeTokenEndpoint {
+
+    @Autowired
+    @Qualifier("consumerTokenServices")
+    ConsumerTokenServices consumerTokenServices;
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/oauth/token")
+    @ResponseBody
+    public String revokeToken(String access_token) {
+        if (consumerTokenServices.revokeToken(access_token)){
+            return "注销成功";
+        }else{
+            return "注销失败";
+        }
+    }
+
+}
